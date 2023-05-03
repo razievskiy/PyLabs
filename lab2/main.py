49bbcf1623483d3e9ddfcc06b98eda4653211419
razievskiy <ilya.razievskij@gmail.com>
2023-05-03 19:20:24 +0300

    lab2

---
+++ lab2/main.py
@@ -0,0 +1,14 @@
+# мультипарадигмальні мови програмування , лаб 2 , Разієвський Ілля
+
+TEMPLATE = 'Enter {} value '
+print('Парадигмальні мови прогр , лаба 2')
+print('Разіэвський Ілля')
+
+x = int(input(TEMPLATE.format('x')))
+
+y = float(input(TEMPLATE.format('y')))
+
+z = float(input(TEMPLATE.format('z')))
+
+result = ((z - 23.1) * (pow(2, x) + 2.2)) / (y - 21.1) - 3.2
+print(result)
